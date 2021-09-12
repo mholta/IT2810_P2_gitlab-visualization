925fd59727b8eab6c5f5b4115546d8636eb3f4c3
Magnus Holta <magnus.holta@gmail.com>
2021-09-12 20:45:46 +0200

    initialize project #1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
+import MainPage from "./pages/mainPage";
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      {/* Put context here */}
+      <MainPage />
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/styles/theme.tsx
@@ -0,0 +1,25 @@
+import { createTheme } from "@material-ui/core/styles";
+
+// Theme component for Material UI
+// This is just overrides, the full theme object contains
+// more styles
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1a3a69",
+    },
+    secondary: {
+      main: "#E69C34",
+    },
+    text: {
+      primary: "#fef2f2",
+      secondary: "lightgrey",
+    },
+  },
+  typography: {
+    fontFamily: ["Source Sans Pro"].join(","),
+  },
+});
+
+export default theme;
