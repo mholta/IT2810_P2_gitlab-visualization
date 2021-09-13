4b267ec8e7b690d0283252363d0e32de5be1aa60
Tormod Nygård <tormodny@stud.ntnu.no>
2021-09-13 14:35:06 +0200

    Fetching of users, commits and issues #3

---
+++ src/api/api.tsx
@@ -0,0 +1,59 @@
+import { rejects } from 'assert';
+
+const endpoint = 'https://gitlab.stud.idi.ntnu.no/api/v4/projects/11839/';
+const header = {
+  method: 'GET',
+  headers: new Headers({
+    Authorization: 'Bearer zKPYXxMgutMm5oXDf1sc',
+    'Content-Type': 'application/json'
+  })
+};
+
+export enum GitLabTypes {
+  Users = 'USERS',
+  Commits = 'COMMITS',
+  Issues = 'ISSUES'
+}
+
+export const fetchUsers = () => {
+  return new Promise<any>((resolve) => {
+    fetch(endpoint + 'users', header)
+      .then((res) => res.json())
+      .then((commits) => resolve(commits))
+      .catch((err) => rejects(err));
+  });
+};
+
+export const fetchCommits = (since: Date, until: Date) => {
+  return new Promise<any>((resolve) => {
+    fetch(
+      endpoint +
+        'repository/commits?' +
+        new URLSearchParams({
+          since: since.toISOString(),
+          until: until.toISOString()
+        }),
+      header
+    )
+      .then((res) => res.json())
+      .then((commits) => resolve(commits))
+      .catch((err) => rejects(err));
+  });
+};
+
+export const fetchIssues = (since: Date, until: Date) => {
+  return new Promise<any>((resolve) => {
+    fetch(
+      endpoint +
+        'issues?' +
+        new URLSearchParams({
+          since: since.toISOString(),
+          until: until.toISOString()
+        }),
+      header
+    )
+      .then((res) => res.json())
+      .then((commits) => resolve(commits))
+      .catch((err) => rejects(err));
+  });
+};
