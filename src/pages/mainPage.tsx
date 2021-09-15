00d6265f740490f0817e193e1c69a7addc373a7e
Tormod Nygård <tormodny@stud.ntnu.no>
2021-09-15 12:41:41 +0200

    Better handling of users #3

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -4,11 +4,13 @@ import SideBar from '../components/sidebar';
 import { withTheme } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { fetchCommits, fetchIssues, fetchUsers, GitLabTypes } from '../api/api';
+import { fetchCommits, fetchIssues, GitLabTypes } from '../api/api';
 import { Main } from '../components/layout';
 
 const MainPage = () => {
   const [invert, setInvert] = useState<boolean>(false);
+  const [users, setUsers] = useState<any>({});
+  const [userNum, setUserNum] = useState<number>(1);
   const [data, setData] = useState<any>({ loading: false, data: null });
   const [filter, setFilter] = useState<any>({
     since: new Date(1609459200000),
@@ -18,16 +20,45 @@ const MainPage = () => {
 
   useEffect(() => {
     setData({ loading: true, data: null });
-    if (filter.type === GitLabTypes.Users) {
-      fetchUsers().then((users) => setData({ loading: false, data: users }));
-    } else if (filter.type === GitLabTypes.Commits) {
-      fetchCommits(filter.since, filter.until).then((commits) =>
-        setData({ loading: false, data: commits })
-      );
+
+    if (filter.type === GitLabTypes.Commits) {
+      fetchCommits(filter.since, filter.until).then((commits) => {
+        const newUsers: any = {};
+        let currentUserNum: number = userNum;
+        commits
+          .map((d: any) => d.author_name)
+          .filter((v: any, i: number, a: any) => a && a.indexOf(v) === i) // https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
+          .forEach((name: any) => {
+            if (name && !(name in users)) {
+              newUsers[name] = 'User ' + currentUserNum;
+              currentUserNum++;
+            }
+          });
+
+        setUsers({ ...users, ...newUsers });
+        setUserNum(currentUserNum);
+
+        setData({ loading: false, data: commits });
+      });
     } else if (filter.type === GitLabTypes.Issues) {
-      fetchIssues(filter.since, filter.until).then((issues) =>
-        setData({ loading: false, data: issues })
-      );
+      fetchIssues(filter.since, filter.until).then((issues) => {
+        const newUsers: any = {};
+        let currentUserNum: number = userNum;
+        issues
+          .map((i: any) => i.assignee?.name)
+          .filter((v: any, i: number, a: any) => a.indexOf(v) === i) // https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
+          .forEach((name: string) => {
+            if (name && !(name in users)) {
+              newUsers[name] = 'User ' + currentUserNum;
+              currentUserNum++;
+            }
+          });
+
+        setUsers({ ...users, ...newUsers });
+        setUserNum(currentUserNum);
+
+        setData({ loading: false, data: issues });
+      });
     }
   }, [filter]);
 
