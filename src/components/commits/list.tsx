22e8a5edc3e689df7016d48a275a80ae63932d5d
Håkon Nese <haaknes@stud.ntnu.no>
2021-09-28 12:05:11 +0200

    Sticky users at top #23

--- src/components/commits/list.tsx
+++ src/components/commits/list.tsx
@@ -51,29 +51,31 @@ const List = ({ commits, users }: ListProps) => {
       : null;
 
   return (
-    <OuterScrollWrapper>
-      <MainWrapper showColumns={showColumns}>
-        <ToggleButtonWrapper>
-          {showColumnToggle && (
-            <IconButtonWithLabel
-              onClick={() => setShowColumns(!showColumns)}
-              icon={showColumns ? <ViewStreamOutlined /> : <ViewWeekOutlined />}
-              label={showColumns ? 'List' : 'Columns'}
-            />
-          )}
-        </ToggleButtonWrapper>
-
-        <ColumnTitlesWrapper columns={columns} showColumns={showColumns}>
-          {showColumns ? (
-            users
-              .filter((u) => u.show)
-              .map((user: User, index: number) => (
+    <MainWrapper showColumns={showColumns}>
+      <InnerScrollWrapper>
+        <TopBarWrapper>
+          <ToggleButtonWrapper>
+            {showColumnToggle && (
+              <IconButtonWithLabel
+                onClick={() => setShowColumns(!showColumns)}
+                icon={
+                  showColumns ? <ViewStreamOutlined /> : <ViewWeekOutlined />
+                }
+                label={showColumns ? 'List' : 'Columns'}
+              />
+            )}
+          </ToggleButtonWrapper>
+          <ColumnTitlesWrapper columns={columns} showColumns={showColumns}>
+            {showColumns ? (
+              users.map((user, index) => (
                 <ColumnTitle key={'c-user-' + index}>{user.alias}</ColumnTitle>
               ))
-          ) : (
-            <ColumnTitle>All {category.toLowerCase()}</ColumnTitle>
-          )}
-        </ColumnTitlesWrapper>
+            ) : (
+              <ColumnTitle>Alle commits</ColumnTitle>
+            )}
+          </ColumnTitlesWrapper>
+        </TopBarWrapper>
+
         <BackgroundWrapper>
           {showColumns &&
             Array.from(Array(columns + 1)).map((e, index) => (
@@ -82,26 +84,24 @@ const List = ({ commits, users }: ListProps) => {
         </BackgroundWrapper>
 
         <ListWrapper columns={columns} showColumns={showColumns}>
-          {commits
-            .filter((u) => u.user.show)
-            .map((commitData, index) => (
-              <CardWrapper
-                columns={columns}
-                showColumns={showColumns}
-                currentColumn={getColumnNum(commitData.user)}
-                key={'commit-wrapper-' + index}
-              >
-                <ListCard
-                  commitData={commitData}
-                  key={'commit-' + index}
-                  openOnClick={!showColumns}
-                  user={getUser(commitData.user)}
-                />
-              </CardWrapper>
-            ))}
+          {commits.map((commitData, index) => (
+            <CardWrapper
+              columns={columns}
+              showColumns={showColumns}
+              currentColumn={getColumnNum(commitData.user)}
+              key={'commit-wrapper-' + index}
+            >
+              <ListCard
+                commitData={commitData}
+                key={'commit-' + index}
+                openOnClick={!showColumns}
+                user={commitData.user}
+              />
+            </CardWrapper>
+          ))}
         </ListWrapper>
-      </MainWrapper>
-    </OuterScrollWrapper>
+      </InnerScrollWrapper>
+    </MainWrapper>
   );
 };
 
@@ -118,6 +118,8 @@ const ToggleButtonWrapper = styled.div`
   position: absolute;
   top: 1rem;
   left: 3%;
+  // position: sticky;
+  z-index: 10;
 `;
 
 const CardWrapper = styled.div<CommitCardWrapperProps>`
@@ -140,6 +142,7 @@ const CardWrapper = styled.div<CommitCardWrapperProps>`
     1fr
   );
   gap: 0 var(--padding);
+  // min-width: 50rem;
 `;
 
 const ListWrapper = styled.div<ColumnsProps>`
@@ -147,7 +150,7 @@ const ListWrapper = styled.div<ColumnsProps>`
   position: relative;
   z-index: 2;
   margin-top: 1rem;
-
+  // overflow: hidden;
   display: grid;
   grid-template-columns: repeat(${(props) => props.columns}, 1fr);
   gap: 0 var(--padding);
@@ -164,21 +167,31 @@ const ColumnTitlesWrapper = withTheme(styled.div<ColumnsProps>`
   gap: 1rem var(--padding);
   padding: 0 calc(var(--padding) / 2);
   border-bottom: 2px solid ${(props) => props.theme.palette.grey[200]};
+  // position: sticky;
+  // top: 0;
+  // z-index: 50;
 `);
+const TopBarWrapper = styled.div`
+  position: sticky;
+  z-index: 10;
+  background-color: white;
+  top: 0;
+`;
 
-const OuterScrollWrapper = styled.div`
-  max-width: 100%;
+const InnerScrollWrapper = styled.div`
   overflow: auto;
+
+  // max-width: 90vw;
+  max-height: 90vh;
 `;
 
 const MainWrapper = withTheme(styled.div<{ showColumns: boolean }>`
   border: 2px solid ${(props) => props.theme.palette.grey[200]};
   border-radius: 1rem;
-
   position: relative;
   overflow: hidden;
-
-  margin-top: 2rem;
+  z-index: 15;
+  // margin-top: 2rem;
 
   --padding: 2rem;
 
@@ -194,6 +207,7 @@ const BackgroundWrapper = withTheme(styled.div`
   display: flex;
   justify-content: space-between;
   pointer-events: none;
+  z-index: 11;
 
   & > span {
     width: 2px;
